9f76cda314b268249a7c1d53d0d1616eb2142e3c
Bl4zej-kb <b.qliberda@gmail.com>
2026-08-16 14:15:09 +0200

    Initial commit

---
+++ SatelliteSim/Display.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Raylib_cs;
+
+public class Display
+{
+    private int X, Y;
+    private string Name;
+
+    public Display(int x, int y, string name, bool Init)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+
+        if (Init)
+        {
+            init();
+        }
+    }
+    
+    public void init()
+    {
+        Raylib.InitWindow(X, Y, Name);
+    }
+
+    public void render()
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.Black);
+        
+        Raylib.DrawCircle(500, 500, 100, Color.Green);
+        
+        Raylib.EndDrawing();
+    }
+}
---
+++ SatelliteSim/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Display display = new Display(1000, 1000, "Satellite Sim", true);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            display.render();
+        }
+    }
+}
